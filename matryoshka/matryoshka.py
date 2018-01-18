36edacda761b5ef5d8cc7ef667df21d1f90b0e23
vinceniko <vincent.nikolayev@gmail.com>
2018-01-18 18:43:22 -0500

    simplified code w/ recursion

--- matryoshka/matryoshka.py
+++ matryoshka/matryoshka.py
@@ -3,23 +3,26 @@ import numpy as np
 
 def unpack_arrays(array, preserve_empties=True, types=[]):
     """
-    Unpacks any array with nested-array type-containers into a single level array (*note: refer to preserve_empties array for the slight exception). DOES NOT unpack strings (ie. 'st' ==> 's','t') - refer to unpack_all_iters for such functionality. If the str type is passed into types, then an endless loop is created. This is because a string of 1 element is endlessly iterated over. unpack_arrays has more real-world use-cases than the unpack_all_iters.
+    Unpacks any array with nested-array type-containers into a single level array (*note: refer to preserve_empties array for the slight exception to this).
 
     Example:
-        in: [[[1],2], 'string',([3],[[4]]), ()]
+        in: [[[1],2],([3],[[4]]), (), 'string']
 
-        out: [1, 2, 'string', 3, 4, ()]
+        out: [1, 2, 3, 4, (), 'string']
 
-        * preserve_empties as False returns [1, 2, 'string', 3, 4]
+        * preserve_empties as False returns [1, 2, 3, 4, 'string']
+
+        *if str is passed into types like [str]. [1, 2, 3, 4, 's', 't', 'r', 'i', 'n', 'g'] is returned
 
     Params:
-        array: input ie. [[[1],2], 'string',([3],[[4]]), ()]
+        array: input
         preserve_empties (bool): whether to append empty iterable such as [] to returned list
         types (list): allowed iter types. in default string absent.
 
     Returns:
         unpacked array ex. [1, 2, 'string', 3, 4]
     """
+
     # select types
     iter_types = [list, tuple, np.ndarray]
     for t in types:
@@ -27,75 +30,31 @@ def unpack_arrays(array, preserve_empties=True, types=[]):
 
     iter_types = tuple(iter_types)  # convert types to tuple
 
-    ###### MAIN ######
-    while True:
-        array_unpacked = []
-        iter_test = False
-        test_elems = []
-
+    # recursive function
+    def recurse(array):
         for elem in array:
             if isinstance(elem, iter_types):
                 try:
-                    elem[0]  # catches exception
-                    array_unpacked.extend(elem)  # extend list with elements within
-                    iter_test = True
-                except Exception:  # no zero element in iterable
+                    if len(elem) == 1 and isinstance(elem, str):  # conditions for str
+                        array_unpacked.append(elem)
+                    else:  # conditions for iters that are not strings
+                        recurse(elem)
+                except TypeError:  # not an iter
                     if preserve_empties:
                         array_unpacked.append(elem)
-                    iter_test = False
-
-            else:  # not an iterable
+            else:
                 array_unpacked.append(elem)
-                iter_test = False
 
-            test_elems.append(iter_test)
+    ### MAIN ###
+    array_unpacked = []
 
-        array = array_unpacked.copy()  # rewrite main array
-
-        if not any(test_elems):  # if no iterables left
-            break
-
-    return array_unpacked
-
-
-def unpack_all_iters(array):
-    """
-    Unpacks any array with sub-iterables into a single array. unpacks strings unlike unpack_arrays function.
-
-    Example:
-    in: [[[1],2], 'string',([3],[[4]]), ()]
-
-    out: [1, 2, 's', 't', 'r', 'i', 'n', 'g', 3, 4]
-
-    Params:
-        array: input
-
-    Returns:
-        unpacked array
-    """
-    while True:
-        array_unpacked = []
-        iter_test = False
-        for elem in array:
-            try:
-                iter(array)  # catches exception
-                array_unpacked.extend(elem)
-                iter_test = True
-            except TypeError:
-                array_unpacked.append(elem)
-                iter_test = False
-
-        array = array_unpacked.copy()
-
-        if not iter_test:
-            break
+    recurse(array)
 
     return array_unpacked
 
 
 if __name__ == '__main__':
-    array = [[[1], 2], 'string', ([3], [[4]]), ()]
+    array = [[[1], 2], ([3], [[4]]), (), 'string']
 
-    print(unpack_arrays(array))
+    print(unpack_arrays(array, types=[str]))
     print(unpack_arrays(array, False))
-    print(unpack_all_iters(array))
